9f733c814182dc5cae9df65586373c34494f4de5
Arnoldo Arturo <arnoldoarturoxd@gmail.com>
2024-05-14 14:31:34 -0600

    added mpsc module

--- src/server/tests.rs
+++ src/server/tests.rs
@@ -1,9 +1,11 @@
-#[allow(unused)]
+#![allow(unused)]
 use crate::server::ServerWrapper;
+use std::sync::mpsc;
 
 #[test]
 fn host_spawn() {
-    let wrapper = ServerWrapper::new();
+    let (tx, _rx) = mpsc::channel();
+    let wrapper = ServerWrapper::new(tx);
     let host_switch = wrapper.server.host_server();
 
     assert_eq!(host_switch, true);
