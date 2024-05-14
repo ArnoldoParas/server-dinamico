c7a1ec793062fcc47402e95c836deaa643bd8dc6
Arnoldo Paras <aparasreyes2@gmail.com>
2024-05-14 16:13:34 -0600

    Added manage_mutex text

--- src/server/tests.rs
+++ src/server/tests.rs
@@ -1,12 +1,28 @@
 #![allow(unused)]
 use crate::server::ServerWrapper;
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
+
+use super::manage_mutex;
+
+// #[test]
+// fn host_spawn() {
+//     let (tx, _rx) = mpsc::channel();
+//     let wrapper = ServerWrapper::new(tx);
+//     let host_switch = wrapper.server.host_server();
+
+//     assert_eq!(host_switch, true);
+// }
 
 #[test]
-fn host_spawn() {
-    let (tx, _rx) = mpsc::channel();
-    let wrapper = ServerWrapper::new(tx);
-    let host_switch = wrapper.server.host_server();
+fn m_mutex() {
+    let mut modify = Arc::new(Mutex::new(false));
+    let see = Arc::new(Mutex::new("Hi".to_string()));
+    // let see_cpy = see.clone();
 
-    assert_eq!(host_switch, true);
+    // manage_mutex(see_cpy, Some("?".to_string()));
+
+    let mut modify_copy = modify.clone();
+    assert_eq!(manage_mutex(modify, None), Some(false));
+    assert_eq!(manage_mutex(modify_copy, Some(true)), Some(true));
+    assert_eq!(manage_mutex(see, Some("Hi :D".to_string())), Some("Hi :D".to_string()));
 }
