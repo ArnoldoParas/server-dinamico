5a648a1c019fb10534695909e0494cbc72ffed94
Arnoldo Arturo <arnoldoarturoxd@gmail.com>
2024-05-14 14:32:07 -0600

    Added handler stream method

--- src/server.rs
+++ src/server.rs
@@ -2,13 +2,15 @@ use std::{
     collections::HashMap, 
     io::{prelude::*, BufReader}, 
     net::{TcpListener, TcpStream}, 
-    sync::{Arc, Mutex}, thread, 
+    sync::{mpsc, Arc, Mutex}, thread, 
     time::Duration
 };
+use uuid::Uuid;
 
 mod tests;
 
 struct Server {
+    sender: mpsc::Sender<String>,
     current_ip: Arc<Mutex<String>>,
     termination_signal: Arc<Mutex<bool>>,
     switch_mode: Arc<Mutex<bool>>,
@@ -29,10 +31,11 @@ impl ServerWrapper {
     ///
     /// let server = ServerWrapper::new();
     /// ```
-    pub fn new() -> ServerWrapper {
+    pub fn new(tx: mpsc::Sender<String>) -> ServerWrapper {
         ServerWrapper {
             server: Arc::new(Server {
-                current_ip: Arc::new(Mutex::new(String::from("10.100.33.76:3012"))),
+                sender: tx,
+                current_ip: Arc::new(Mutex::new(String::from("10.100.38.190:3012"))),
                 termination_signal: Arc::new(Mutex::new(false)),
                 switch_mode: Arc::new(Mutex::new(false))
             }),
@@ -42,7 +45,7 @@ impl ServerWrapper {
     pub fn run(&self) {
         let server_clone_1 = self.server.clone();
         thread::spawn(move || {
-            let mut state = true;
+            let mut state = false;
             loop {
                 let server_clone_2 = server_clone_1.clone();
                 match state {
@@ -76,8 +79,8 @@ impl Server {
             let guard = ip_clone.lock().unwrap();
             ip = String::from(&*guard);
         };
-        let mut request = String::from(" \nH1\nH2");
-
+        let mut request = String::from("NoId\nH1\nH2");
+        
         loop {
             let mut stream;
             match TcpStream::connect(&ip) {
@@ -99,19 +102,31 @@ impl Server {
                 .take_while(|line| !line.is_empty())
                 .collect();
 
-            if http_response[0] != "OK" {
-                request = format!("{}\nH1\nH2", http_response[2]);
+            let http_response = {
+                let mut temp_hashmap = HashMap::new();
+                for element in http_response {
+                    let (k, v) = element
+                        .split_once(':')
+                        .unwrap();
+                    temp_hashmap.insert(k.to_owned(), v.trim().to_owned());
+                }
+                println!("{:#?}", temp_hashmap);
+                temp_hashmap
+            };
+            // println!("Response: {:#?}", http_response);
+
+            if http_response.get("State").unwrap() != "OK" {
+                request = format!("{}\nH1\nH2", http_response.get("Id").unwrap());
             }
-            if http_response[1] != "None" {
+            if http_response.get("SwitchToServer").unwrap() == "true" {
                 {
                     let ip_clone = self.current_ip.clone();
                     let mut guard = ip_clone.lock().unwrap();
-                    *guard = format!("{}:3012", &http_response[1]);
+                    *guard = format!("{}:3012", stream.local_addr().unwrap().ip().to_string()); //posible fallo
                 }
                 server_mode_switch = true;
                 break;
             }
-            println!("Response: {:#?}", http_response);
             thread::sleep(Duration::from_millis(1500));
         }
         server_mode_switch
@@ -141,20 +156,88 @@ impl Server {
 
             let stream = stream.expect("Fallo en inicial el stream");
             let guard = self.switch_mode.lock().unwrap();
-            if *guard{
-                // switch_connection(stream, &mut hosts, ip_clone);
-                continue; 
-            };
-            // handle_conecction(stream, &mut hosts);
+            match *guard {
+                true => {
+                    self.handle_conecction(stream, &mut hosts_dir, true);
+                    continue;
+                    },
+                false => self.handle_conecction(stream, &mut hosts_dir, false)
+            }
         }
-
         false
     }
 
     fn pulse(&self) {
-        loop {
-            println!("hi from pulse");
-            thread::sleep(Duration::from_secs(1));
+        println!("hi from pulse");
+        let current_ip;
+        {
+            let guard = self.current_ip.lock().unwrap();
+            current_ip = String::from(&*guard);
         }
+        loop {
+            thread::sleep(Duration::from_secs(15));
+            { // If server switch
+                let mut guard = self.switch_mode.lock().unwrap();
+                *guard = true;
+            }
+            thread::sleep(Duration::from_secs(2));
+
+            let mut signal = self.termination_signal.lock().unwrap();
+            *signal = true;
+            let mut stream = TcpStream::connect(&current_ip).unwrap();
+            stream.write_all("OK\nNone\n".as_bytes()).unwrap(); // probably change request
+
+            break;
+        }
+    }
+
+    fn handle_conecction(&self, mut stream: TcpStream, hosts_dir: &mut HashMap<String, String>, migration: bool) {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        println!("Request: {:#?}", http_request);
+
+        let response;
+
+        match migration {
+            true => {
+                let host_ip = stream
+                    .peer_addr()
+                    .unwrap()
+                    .ip()
+                    .to_string();
+                if host_ip == hosts_dir.get(&http_request[0]).unwrap().to_owned() {
+                    let new_ip = stream.peer_addr().unwrap().ip().to_string();
+                    response = format!("State: OK\nSwitchToServer: true\nNewServer: None\nId: {}", http_request[0]);
+
+                    let mut guard = self.current_ip.lock().unwrap();
+                    *guard = format!("{}:3012",new_ip);
+                } else {
+                    response = format!("State: Unauthorized\nSwitchToServer: false\nNewServer: {}\nId: {}", host_ip, http_request[0]);
+                }
+            },
+            false => {
+                if http_request[0] == "NoId" {
+                    let id = String::from(Uuid::new_v4());
+                    response = format!("State: Unauthorized\nSwitchToServer: false\nNewServer: None\nId: {}", id);
+                    hosts_dir.insert(
+                        id, 
+                        stream
+                            .peer_addr()
+                            .unwrap()
+                            .ip()
+                            .to_string()
+                    );
+                } else {
+                    response = format!("State: OK\nSwitchToServer: false\nNewServer: None\nId: {}", http_request[0]);
+                    println!("----------\nhost ip: {}\n----------\n",stream.peer_addr().unwrap());
+                }
+            }
+        }
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
