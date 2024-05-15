982e6243b96a701345c27fa4a63ca24c56ccdf85
Arnoldo Paras <aparasreyes2@gmail.com>
2024-05-15 01:36:13 -0600

    Clean old WebSocket code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
 #![allow(unused)]
 use server::app::App;
 use server::server::ServerWrapper;
-use std::{net::TcpListener, sync::mpsc::{self, Receiver, Sender}, thread, time::Duration};
-// use tungstenite::accept;
+use std::{net::TcpListener, 
+    sync::mpsc::{self, Receiver, Sender}, 
+    thread, time::Duration};
 
 fn main() -> eframe::Result<()>{
-    let (tx, rx) = mpsc::channel(); //: (Sender<Vec<String>>, Receiver<String>)
+    let (tx, rx) = mpsc::channel();
+
+    let server = ServerWrapper::new(tx);
+    server.run();
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1100.0, 550.0]),
         ..Default::default()
     };
-
-    // let listener = TcpListener::bind("0.0.0.0:5432").unwrap();
-    // let listener_clone = listener.try_clone().expect("Failed to clone listener");
-    // thread::spawn(move || {
-    //     tcp_listener_thread(listener_clone, tx)
-    // });
-    let server = ServerWrapper::new(tx);
-    server.run();
 
     eframe::run_native(
         "status",
@@ -26,38 +22,3 @@ fn main() -> eframe::Result<()>{
         Box::new(|cc| Box::new(App::new(cc, rx))),
     )
 }
-
-// fn tcp_listener_thread(listener: TcpListener, tx: mpsc::Sender<String>) {
-//     let mut id: u32 = 0;
-//     for stream in listener.incoming() {
-//         let ws = accept(stream.unwrap()).expect("Failed to accept");
-//         let tx_clone = tx.clone();
-//         thread::spawn(move || handle_connection(ws, tx_clone, id));
-//         id += 1;
-//     }
-// }
-
-// fn handle_connection(
-//     mut ws: tungstenite::WebSocket<std::net::TcpStream>,
-//     tx: mpsc::Sender<String>,
-//     id: u32,
-// ) {
-//     let mut register = String::from("");
-//     loop {
-//         let msg = match ws.read() {
-//             Ok(msg) => msg,
-//             Err(_) => {
-//                 let msg = tungstenite::Message::Text(register);
-//                 let msg = format!("{id},{msg},disconnected");
-//                 eprintln!("Host disconnected");
-//                 println!("{msg}");
-//                 tx.send(msg).unwrap();
-//                 thread::sleep(Duration::from_secs(1));
-//                 break;
-//             }
-//         };
-//         register = msg.to_string().to_owned();
-//         let msg = format!("{id},{},connected", msg);
-//         tx.send(msg).unwrap();
-//     }
-// }
