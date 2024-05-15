26a7bee01df5ccfeac830b6acad2de53404711d3
Arnoldo Paras <aparasreyes2@gmail.com>
2024-05-15 04:22:08 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,20 @@ use std::{
     time::Duration,
 };
 
-fn main()  {// -> eframe::Result<()>
-    let (tx, _rx) = mpsc::channel();
+fn main() -> eframe::Result<()>{
+    let (tx, rx) = mpsc::channel();
 
     let server = ServerWrapper::new(tx);
     server.run();
 
-    // let native_options = eframe::NativeOptions {
-    //     viewport: egui::ViewportBuilder::default().with_inner_size([1100.0, 550.0]),
-    //     ..Default::default()
-    // };
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1100.0, 550.0]),
+        ..Default::default()
+    };
 
-    // eframe::run_native(
-    //     "status",
-    //     native_options,
-    //     Box::new(|cc| Box::new(App::new(cc, rx))),
-    // )
-    loop{}
+    eframe::run_native(
+        "status",
+        native_options,
+        Box::new(|cc| Box::new(App::new(cc, rx))),
+    )
 }
