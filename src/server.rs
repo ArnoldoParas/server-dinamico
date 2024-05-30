73d89269411334a3f6226230c8e2e17e9e328950
Arnoldo Paras <aparasreyes2@gmail.com>
2024-05-29 18:35:24 -0600

    .

--- src/server.rs
+++ src/server.rs
@@ -308,12 +308,12 @@ impl Server {
                     let mut fecha_datetime = DateTime::parse_from_str(&value[7].as_str(), "%+") // %Y-%m-%d %H:%M:%S%.f %Z
                         .unwrap()
                         .with_timezone(&Utc);
-                    let diff = (pulse_time - fecha_datetime).num_seconds();
+                    let diff = (dbg!(pulse_time) - dbg!(fecha_datetime)).num_seconds();
 
                     if value.len() == 9 {
                         value.pop();
                     }
-                    if dbg!(dbg!(dbg!(diff)) >= 6) {
+                    if dbg!(diff >= 6) {
                         value.push(String::from("disconnected"));
                     } else {
                         value.push(String::from("connected"));
